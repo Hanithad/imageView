fe69412f325ca84e1f9a86b737dbf03fc488ad64
Hanithad <dhanitha92@gmail.com>
2024-03-18 17:15:54 -0500

    adding constraints to the view

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelName.text = ""
     }
     
     @IBAction func showMessagedTapped(_ sender: Any) {
