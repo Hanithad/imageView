c306868249c30700e2570b909c51c323c1a23ecc
Hanithad <dhanitha92@gmail.com>
2024-03-18 13:13:27 -0500

    New changes1

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showMessagedTapped(_ sender: Any) {
+        labelName.text = "Hi There"
+        labelName.textColor = .cyan
+    }
+    
 
 
 }
