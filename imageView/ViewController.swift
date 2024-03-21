a3fe539ec941f1e3740a08baa99c829f4b4bc658
Hanithad <dhanitha92@gmail.com>
2024-03-21 08:46:31 -0500

    added an array and changing the message when button tapped

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelName: UILabel!
     @IBOutlet weak var imagePicture: UIImageView!
     
+    
+    var imageNumber = 0
+    var messageNmunber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,11 +23,42 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessagedTapped(_ sender: Any) {
-        labelName.text = "Hi There"
-        imagePicture.image = UIImage(named: "image1")
+        var messages = ["Namaste", "welcome to India", "How are you today"]
+        
+        messages.append("You are beautiful")
+        messages.remove(at: 0)
+        
+        labelName.text = messages.randomElement()
+//        labelName.text = messages[messageNmunber]
+//        messageNmunber += 1
+//        if messageNmunber == 3{
+//            messageNmunber = 0
+//        }
+        print(imageNumber)
+        imagePicture.image = UIImage(named: "image\(imageNumber)")
+        imageNumber += 1
+        if imageNumber == 3{
+            imageNumber = 0
+        }
+        
+        
+//        var thereMessage = "Hi There"
+//    
+//        if labelName.text == thereMessage {
+//            labelName.text = "you are awesome"
+//            imagePicture.image = UIImage(named: "image1")
+//        } else if labelName.text == "you are awesome" {
+//            labelName.text = "you are super"
+//            imagePicture.image = UIImage(named: "image2")
+//        }else{
+//            labelName.text = thereMessage
+//            imagePicture.image = UIImage(named: "image0")
+//        }
     }
+        
+}
     
 
 
-}
+
 
